8796e7ae6fda5ce47bf1a5351ee656a60feb1883
Jelle van der Waa <jelle@vdwaa.nl>
2016-12-04 17:39:58 +0100

    Handle [testing] enabled situation

--- arch-audit.py
+++ arch-audit.py
@@ -16,32 +16,39 @@ def main(options):
     configpath = '/etc/pacman.conf'
     handle = pycman.config.init_with_config(configpath)
     db = handle.get_localdb()
+    # Handle the situation where a user has enabled [testing]
+    if len([syncdb for syncdb in handle.get_syncdbs() if syncdb.name == 'testing']):
+        status = ['Testing', 'Fixed']
+    else:
+        status = ['Fixed']
 
     # Fetch latest JSON API
     r = requests.get(API_URL + '/json')
     data = r.json()
 
     for avg in data:
-        packages = "|".join(avg['packages'])
-        pkgs = db.search('^({})$'.format(packages))
+        search_str = '^({})$'.format('|'.join(avg['packages']))
+        pkgs = db.search(search_str)
 
         if not pkgs:
             continue
 
         # for every pkg, check if affected
         for pkg in pkgs:
-            if not avg['fixed'] and avg['status'] == 'Vulnerable':
+            if avg['status'] == 'Vulnerable':
                 if not options.upgrade:
                     print('{}-{} is vulnerable to {}'.format(pkg.name, pkg.version, avg['type']))
                     print('No fixed package in the repositories.')
                     print('AVG: {}/{}'.format(API_URL, avg['name']))
                     print('')
-            elif avg['fixed'] and vercmp(pkg.version, avg['fixed']) < 0:
-                print('{}-{} is vulnerable to {}'.format(pkg.name, pkg.version, avg['type']))
-                print('Upgrade to {}'.format(avg['fixed']))
-                for advisory in avg['advisories']:
-                    print('Advisory: {}/{}/generate/raw'.format(API_URL, advisory))
-                print('')
+            elif avg['status'] in status:
+                if vercmp(pkg.version, avg['fixed']) < 0:
+                    print('{}-{} is vulnerable to {}'.format(pkg.name, pkg.version, avg['type']))
+                    print('Upgrade to {}'.format(avg['fixed']))
+                    print('AVG: {}/{}'.format(API_URL, avg['name']))
+                    for advisory in avg['advisories']:
+                        print('Advisory: {}/{}/generate/raw'.format(API_URL, advisory))
+                    print('')
 
 
 def parse_args():

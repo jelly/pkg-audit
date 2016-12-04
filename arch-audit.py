07010dd3914474f2a8fa91438d959f48b1bf73c0
Jelle van der Waa <jelle@vdwaa.nl>
2016-12-04 17:06:05 +0100

    Streamline the output.

--- arch-audit.py
+++ arch-audit.py
@@ -30,11 +30,18 @@ def main(options):
 
         # for every pkg, check if affected
         for pkg in pkgs:
-            if not avg['fixed']:
+            if not avg['fixed'] and avg['status'] == 'Vulnerable':
                 if not options.upgrade:
-                    print('Package {} is vulnerable to {}, check the tracker for workarounds for {}'.format(pkg.name, avg['type'], avg['name']))
-            elif vercmp(pkg.version, avg['fixed']) < 0:
-                print('Package {} is vulnerable to {}, upgrade to {}'.format(pkg.name, avg['type'], avg['fixed']))
+                    print('{}-{} is vulnerable to {}'.format(pkg.name, pkg.version, avg['type']))
+                    print('No fixed package in the repositories.')
+                    print('AVG: {}/{}'.format(API_URL, avg['name']))
+                    print('')
+            elif avg['fixed'] and vercmp(pkg.version, avg['fixed']) < 0:
+                print('{}-{} is vulnerable to {}'.format(pkg.name, pkg.version, avg['type']))
+                print('Upgrade to {}'.format(avg['fixed']))
+                for advisory in avg['advisories']:
+                    print('Advisory: {}/{}/generate/raw'.format(API_URL, advisory))
+                print('')
 
 
 def parse_args():

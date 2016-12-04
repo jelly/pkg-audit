ff16aa2c6b16fa0e8221b2f83201fd7791ec55f7
Jelle van der Waa <jelle@vdwaa.nl>
2016-12-04 16:39:13 +0100

    Add command line option support

--- arch-audit.py
+++ arch-audit.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python
 
+import argparse
 import requests
 import pycman
 from pyalpm import vercmp
 
 
-# Initialize pyalpm
-configpath = '/etc/pacman.conf'
-handle = pycman.config.init_with_config(configpath)
-db = handle.get_localdb()
+def main(options):
+    # Initialize pyalpm
+    configpath = '/etc/pacman.conf'
+    handle = pycman.config.init_with_config(configpath)
+    db = handle.get_localdb()
 
-# Fetch latest JSON API
-r = requests.get('http://45.32.158.89/json')
-data = r.json()
+    # Fetch latest JSON API
+    r = requests.get('http://45.32.158.89/json')
+    data = r.json()
 
-for avg in data:
-    packages = "|".join(avg['packages'])
-    pkgs = db.search('^({})$'.format(packages))
+    for avg in data:
+        packages = "|".join(avg['packages'])
+        pkgs = db.search('^({})$'.format(packages))
 
-    if not pkgs:
-        continue
+        if not pkgs:
+            continue
 
-    # for every pkg, check if affected
-    for pkg in pkgs:
-        if not avg['fixed']:
-            print('Package {} is vulnerable to {}, check the tracker for workarounds for {}'.format(pkg.name, avg['type'], avg['name']))
-        elif vercmp(pkg.version, avg['fixed']) < 0:
-            print('Package {} is vulnerable to {}, upgrade to {}'.format(pkg.name, avg['type'], avg['fixed']))
+        # for every pkg, check if affected
+        for pkg in pkgs:
+            if not avg['fixed']:
+                if not options.upgradeable:
+                    print('Package {} is vulnerable to {}, check the tracker for workarounds for {}'.format(pkg.name, avg['type'], avg['name']))
+            elif vercmp(pkg.version, avg['fixed']) < 0:
+                print('Package {} is vulnerable to {}, upgrade to {}'.format(pkg.name, avg['type'], avg['fixed']))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='audit installed packages against known vulnerabilities')
+    parser.add_argument('--upgradeable', dest='upgradeable', action='store_true', help='Filter on packages which vulernablilties are fixed by performing a system upgrade')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(parse_args())
+    
